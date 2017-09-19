3163e7f4dc895281db45820e78e2d93af69a736e
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2017-09-19 21:10:55 +0300

    Added naive implementation

---
+++ CodeDojo.CircuitBreaker.Tests/CircuitBreaker.cs
@@ -0,0 +1,53 @@
+﻿namespace CodeDojo.CircuitBreaker.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// As a systems maintainer
+    /// I want my calls to be resilient to failure
+    /// So I don't need to turn features on and off
+
+    /// - Implement the circuit breaker pattern
+    ///    - Fault tolerance should be configurable
+    ///    - Half-open timeouts should be configurable
+    /// </summary>
+    [TestFixture]
+    public class CircuitBreaker
+    {
+
+        /// <summary>
+        /// As a developer
+        /// I want to count failures when I make calls
+        /// A failure count is incremented
+        /// </summary>
+        [Test]
+        public void MonitoringFailure()
+        {
+
+        }
+
+        /// <summary>
+        /// As a developer
+        /// When my failure count exceeds a configured tolerance
+        /// All subsequent calls throw an exception and block the call
+        /// </summary>
+        [Test]
+        public void OpeningCircuit()
+        {
+            
+        }
+
+        /// <summary>
+        /// As a developer
+        /// When my code is blocking calls and a configured timeout passes
+        /// I allow a single call to proceed
+        /// - Single call succeeding closes circuit
+        /// - Single call failing resets some kind of timeout
+        /// </summary>
+        [Test]
+        public void HalfOpenedState()
+        {
+
+        }
+    }
+}
---
+++ CodeDojo.CircuitBreaker/CircuitBreaker.cs
@@ -0,0 +1,81 @@
+﻿namespace CodeDojo.CircuitBreaker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public class CircuitBreaker
+    {
+        private enum CircuitState
+        {
+            Closed,
+            Open,
+            HalfOpen
+        }
+
+        private readonly Func<Task> protectedFunction;
+
+        private readonly int failuresThreshold;
+
+        private readonly TimeSpan halfOpenTimeout;
+        private readonly Stopwatch halfOpenTimer = new Stopwatch();
+
+        private readonly List<Exception> exceptions;
+
+        public CircuitBreaker(Func<Task> protectedFunction, int failuresThreshold, TimeSpan halfOpenTimeout)
+        {
+            if (failuresThreshold <= 0)
+            {
+                throw new ArgumentException("Failure threshold must be a positive number (1 for 'no failures allowed')", nameof(failuresThreshold));
+            }
+
+            this.protectedFunction = protectedFunction;
+            this.failuresThreshold = failuresThreshold;
+            this.halfOpenTimeout = halfOpenTimeout;
+            this.exceptions = new List<Exception>(failuresThreshold);
+        }
+
+        public async Task ExecuteAsync()
+        {
+            this.ThrowIfCircuitOpen();
+
+            try
+            {
+                await this.protectedFunction().ConfigureAwait(false);
+                this.CloseCircuit();
+            }
+            catch (Exception ex)
+            {
+                this.NoteErrorOccurrence(ex);
+                this.ThrowIfCircuitOpen();
+            }
+        }
+
+        private void CloseCircuit()
+        {
+            this.exceptions.Clear();
+            this.halfOpenTimer.Reset();
+        }
+
+        private void NoteErrorOccurrence(Exception exception)
+        {
+            this.exceptions.Add(exception);
+            this.halfOpenTimer.Restart();
+        }
+
+        private void ThrowIfCircuitOpen()
+        {
+            if (this.halfOpenTimer.Elapsed > this.halfOpenTimeout)
+            {
+                return;
+            }
+
+            if (this.exceptions.Count >= this.failuresThreshold)
+            {
+                throw new AggregateException(this.exceptions);
+            }
+        }
+    }
+}
